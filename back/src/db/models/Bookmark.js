493bcf15af2904d73173e423528735484a4368a3
brilence75 <brilence75@gmail.com>
2022-04-26 13:33:39 +0900

    Add : Bookmark 모델 구현

---
+++ back/src/db/models/Bookmark.js
@@ -0,0 +1,35 @@
+import {bookmarkModel} from "../schemas/bookmark.js";
+
+class Bookmark {
+  static async createBookmark(Bookmark) {
+    const newBookmark = await bookmarkModel.create();
+    return newBookmark;
+  }
+
+  static async findBookmarkById(BookmarkId) {
+    const bookmark = await bookmarkModel.findOne({id: BookmarkId});
+    return bookmark;
+  }
+
+/**
+ * 
+ * @param {*} userId 
+ * userId로 bookmark 리스트 찾아서 반환
+ */
+  static async findBookmarkByuserId(userId) {
+    const bookmarkList = await bookmarkModel.find({userId});
+    return bookmarkList;
+  }
+
+/**
+ * 
+ * @param {*} bookmarkId 
+ * bookmark 컬렉션에서 bookmarkId와 매칭되는 document 하나를 삭제하는 함수
+ */
+  static async deleteBookmarkById(BookmarkId) {
+    const deleteResult = await bookmarkModel.deleteOne({id: BookmarkId});
+    return deleteResult
+  }
+}
+
+export {Bookmark};
