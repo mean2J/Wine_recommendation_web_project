69268337bd3ace63d356409fc94d87a689f330ff
brilence75 <brilence75@gmail.com>
2022-04-27 10:46:56 +0900

    Docs : bookmark 모델 주석 추가

--- back/src/db/models/Bookmark.js
+++ back/src/db/models/Bookmark.js
@@ -1,34 +1,38 @@
 import {bookmarkModel} from "../schemas/bookmark.js";
 
 class Bookmark {
+/**
+ * Bookmark 객체 생성
+ */
   static async createBookmark(Bookmark) {
     const newBookmark = await bookmarkModel.create(Bookmark);
     return newBookmark;
   }
-
+/**
+ * BookmarkId(=id)로 bookmark 리스트에 존재하는지 확인
+ */
   static async findBookmarkById(BookmarkId) {
     const bookmark = await bookmarkModel.findOne({id: BookmarkId});
     return bookmark;
   }
-
+/**
+ * wineId로 해당와인이 bookmark 리스트에 존재하는지 확인
+ */
   static async findBookmarkByWineId(wineId) {
     const bookmark = await bookmarkModel.findOne({wineId});
     return bookmark;
   }
 
 /**
- * 
  * userId로 bookmark 리스트 찾아서 반환
  */
   static async findBookmarkByUserId(userId) {
     const bookmarkList = await bookmarkModel.find({userId});
-    console.log(bookmarkList)
     return bookmarkList;
   }
 
 /**
- *  
- * bookmark 컬렉션에서 bookmarkId와 매칭되는 document 하나를 삭제하는 함수
+ * bookmarkId와 매칭되는 document 하나를 삭제
  */
   static async deleteBookmarkById(BookmarkId) {
     const deleteResult = await bookmarkModel.deleteOne({id: BookmarkId});
