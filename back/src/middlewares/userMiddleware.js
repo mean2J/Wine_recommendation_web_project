94ae1d2654dca021d7474fee5320877a8015e964
Jaeyeon Kim <fobbb77@gmail.com>
2022-04-27 17:39:27 +0900

    Add req body validation and GET methods to user MVP

--- back/src/middlewares/userMiddleware.js
+++ back/src/middlewares/userMiddleware.js
@@ -1,2 +1,16 @@
-import {checkSchema} from "express-validator";
+import {validationResult} from "express-validator";
 
+function validationErrorCatcher(req, res, next) {
+  const errors = validationResult(req);
+
+  if (errors.isEmpty())
+    return next();
+
+  const error = new Error(
+    errors.array()[0].msg
+  );
+  error.status = 400;
+  throw error;
+}
+
+export {validationErrorCatcher};
