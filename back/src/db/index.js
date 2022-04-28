212f020fd87c301b720e9c37ff40d4c97a3cd8f1
Lee Ha-eun <lheun99@gmail.com>
2022-04-28 15:46:05 +0900

    Merge : feature/back_wineInfo 브랜치 merge

--- back/src/db/index.js
+++ back/src/db/index.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
-import { User } from "./models/User.js";
+import { User } from "./models/user.js";
 import { Wine } from "./models/wine.js";
 import dotenv from "dotenv";
 
