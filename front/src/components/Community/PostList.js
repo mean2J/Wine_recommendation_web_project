78cda09f30fca54e9dad1de6b81197ada9c0d143
KimByeolhee <qufgml0216@naver.com>
2022-05-06 22:31:18 +0900

    Fix: 불필요한 코드 삭제

--- front/src/components/Community/PostList.js
+++ front/src/components/Community/PostList.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useCallback, useContext } from "react";
+import React, { useEffect, useState, useCallback } from "react";
 import { Link, useNavigate, useLocation } from "react-router-dom";
 import { List, Space, Tag, Pagination } from "antd";
 import styled from "styled-components";
